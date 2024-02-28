9078d280d469f04a84ab9ffab6051817646d7010
Yamanaka78 <jjbdumujpgm@gmail.com>
2024-02-28 16:24:54 +0900

    トップ画面の記事詳細処理の作成

--- src/laranews/routes/web.php
+++ src/laranews/routes/web.php
@@ -24,6 +24,9 @@ require __DIR__.'/auth.php';
 Route::get('/', [TopController::class, 'top'])
     ->name('top');
 
+//総合トップ記事詳細画面
+Route::get('/article/{post_id}', [TopController::class, 'articleShow'])->name('top.article.show');
+
 // マイページ
 Route::get('/user/{id}/index', [PostController::class, 'index'])
     ->name('user.index');
